10b2ca6cc3e61c9ff66ab0e7c5ef210601f61d29
Maike Sala <maikesala49@gmail.com>
2023-09-04 19:20:33 -0300

    Voto pra vereador e prefeito finalizado

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos : [
+            {
+                numero: '38111',
+                nome: 'Capitão America',
+                partido: 'ABC',
+                fotos:[
+                    {url:'capitao.jpeg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome:'Viuva Negra',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'viuva.jpeg', legenda:'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:'99',
+                nome:'Rocket',
+                partido: 'ABC',
+                vice:' Thor',
+                fotos: [
+                    {url:'rocket.jpeg', legenda:'Prefeito'},
+                    {url:'thor.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'84',
+                nome:'Home de Ferro',
+                partido: 'QWERTY',
+                vice:' Formigão',
+                fotos: [
+                    {url:'homemdeferro.jpeg', legenda:'Prefeito'},
+                    {url:'formigao.jpeg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+        ]
+    }
+];
